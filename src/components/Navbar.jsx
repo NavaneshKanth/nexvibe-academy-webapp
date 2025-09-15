391a5cc0b1831b593aa92239720f069a28fff231
NavaneshKanth <navaneshkanth@gmail.com>
2025-09-16 01:40:03 +0400

    complete all except seo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = ({ onLoginClick, onRegisterClick }) => {
             <a
               key={item.href}
               href={item.href}
-              className="relative px-3 py-2 text-[var(--color-text-body)] font-semibold drop-shadow-md
+              className="relative px-3 py-2 text-[var(--color-text-body)] font-semibold drop-shadow-md text-nowrap
                 hover:text-[var(--color-secondary)] transition-colors group"
             >
               <span>{item.label}</span>
